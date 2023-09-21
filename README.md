03f82cf6b18ec8ef0781b298be9861887e65dc97
lofiwatercat <alan001tk@gmail.com>
2023-09-20 18:01:10 -0700

    Add readme and swapchain

---
+++ README.md
@@ -0,0 +1,1 @@
+Small project for me to learn ash and vulkan. Following the ashen-aetna tutorial, but adapted for wayland.
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ use ash::version::DeviceV1_0;
 use ash::version::EntryV1_0;
 use ash::version::InstanceV1_0;
 use ash::vk;
+use winit::platform::wayland::WindowExtWayland;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let eventloop = winit::event_loop::EventLoop::new();
+    let window = winit::window::Window::new(&eventloop)?;
+
     let entry = ash::Entry::new()?;
     let enginename = std::ffi::CString::new("BlueMoonEngine").unwrap();
     let appname = std::ffi::CString::new("The Black Window").unwrap();
@@ -16,7 +20,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let layer_names: Vec<std::ffi::CString> =
         vec![std::ffi::CString::new("VK_LAYER_KHRONOS_validation").unwrap()];
     let layer_name_pointers: Vec<*const i8> = layer_names.iter().map(|layer_name| layer_name.as_ptr()).collect();
-    let extension_name_pointers: Vec<*const i8> = vec![ash::extensions::ext::DebugUtils::name().as_ptr()];
+    let extension_name_pointers: Vec<*const i8> = vec![
+        ash::extensions::ext::DebugUtils::name().as_ptr(),
+        ash::extensions::khr::Surface::name().as_ptr(),
+        ash::extensions::khr::WaylandSurface::name().as_ptr(),
+    ];
 
     let mut debugcreateinfo = vk::DebugUtilsMessengerCreateInfoEXT::builder()
         .message_severity(
@@ -43,6 +51,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let utils_messenger = unsafe { debug_utils.create_debug_utils_messenger(&debugcreateinfo, None)? };
 
+    let wayland_display = window.wayland_display().unwrap();
+    let wayland_surface = window.wayland_display().unwrap();
+    let wayland_create_info = vk::WaylandSurfaceCreateInfoKHR::builder()
+        .surface(wayland_surface)
+        .display(wayland_display as *mut vk::wl_display);
+    let wayland_surface_loader = ash::extensions::khr::WaylandSurface::new(&entry, &instance);
+    let surface = unsafe { wayland_surface_loader.create_wayland_surface(&wayland_create_info, None) }?;
+    let surface_loader = ash::extensions::khr::Surface::new(&entry, &instance);
+
+
     let phys_devs = unsafe { instance.enumerate_physical_devices()? };
     let (physical_device, physical_device_properties) = {
         let mut chosen = None;
@@ -61,7 +79,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut found_graphics_q_index = None;
         let mut found_transfer_q_index = None;
         for (index, qfam) in queuefamilyproperties.iter().enumerate() {
-            if qfam.queue_count > 0 && qfam.queue_flags.contains(vk::QueueFlags::GRAPHICS) {
+            if qfam.queue_count > 0 && qfam.queue_flags.contains(vk::QueueFlags::GRAPHICS) && unsafe {
+                surface_loader.get_physical_device_surface_support(physical_device, index as u32, surface)?
+                } {
                 found_graphics_q_index = Some(index as u32);
             }
             if qfam.queue_count > 0 && qfam.queue_flags.contains(vk::QueueFlags::TRANSFER) {
@@ -87,21 +107,79 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .queue_priorities(&priorities)
             .build(),
     ];
+    let device_extension_name_pointers: Vec<*const i8> =
+        vec![ash::extensions::khr::Swapchain::name().as_ptr()];
     let device_create_info = vk::DeviceCreateInfo::builder()
         .queue_create_infos(&queue_infos)
+        .enabled_extension_names(&device_extension_name_pointers)
         .enabled_layer_names(&layer_name_pointers);
     let logical_device =
         unsafe { instance.create_device(physical_device, &device_create_info, None)? };
     let graphics_queue = unsafe { logical_device.get_device_queue(qfamindices.0, 0)};
     let transfer_queue = unsafe { logical_device.get_device_queue(qfamindices.1, 0)};
 
+    let surface_capabilities = unsafe {
+        surface_loader.get_physical_device_surface_capabilities(physical_device, surface)
+    }?;
+    let surface_present_modes = unsafe {
+        surface_loader.get_physical_device_surface_present_modes(physical_device, surface)
+    }?;
+
+    let surface_formats = 
+        unsafe {surface_loader.get_physical_device_surface_formats(physical_device, surface) 
+    }?;
+    dbg!(&surface_capabilities);
+    dbg!(&surface_present_modes);
+    dbg!(&surface_formats);
+    let queuefamilies = [qfamindices.0];
+    let swapchain_create_info = vk::SwapchainCreateInfoKHR::builder()
+        .surface(surface)
+        .min_image_count(
+            3.max(surface_capabilities.min_image_count)
+                .min(surface_capabilities.max_image_count),
+        )
+        .image_format(surface_formats.first().unwrap().format)
+        .image_color_space(surface_formats.first().unwrap().color_space)
+        .image_extent(surface_capabilities.current_extent)
+        .image_array_layers(1)
+        .image_usage(vk::ImageUsageFlags::COLOR_ATTACHMENT)
+        .image_sharing_mode(vk::SharingMode::EXCLUSIVE)
+        .queue_family_indices(&queuefamilies)
+        .pre_transform(surface_capabilities.current_transform)
+        .composite_alpha(vk::CompositeAlphaFlagsKHR::OPAQUE)
+        .present_mode(vk::PresentModeKHR::FIFO);
+    let swapchain_loader = ash::extensions::khr::Swapchain::new(&instance, &logical_device);
+    let swapchain = unsafe { swapchain_loader.create_swapchain(&swapchain_create_info, None)? };
+    let swapchain_images = unsafe { swapchain_loader.get_swapchain_images(swapchain)? };
+    let mut swapchain_imageviews = Vec::with_capacity(swapchain_images.len());
+    for image in &swapchain_images {
+        let subresource_raneg = vk::ImageSubresourceRange::builder()
+            .aspect_mask(vk::ImageAspectFlags::COLOR)
+            .base_mip_level(0)
+            .level_count(1)
+            .base_array_layer(0)
+            .layer_count(1);
+        let imageview_create_info = vk::ImageViewCreateInfo::builder()
+            .image(*image)
+            .view_type(vk::ImageViewType::TYPE_2D)
+            .format(vk::Format::B8G8R8_UNORM)
+            .subresource_range(*subresource_raneg);
+        let imageview = unsafe { logical_device.create_image_view(&imageview_create_info, None) }?;
+        swapchain_imageviews.push(imageview);
+    }
+    
 
     unsafe { 
+        for iv in &swapchain_imageviews {
+            logical_device.destroy_image_view(*iv, None);
+        }
+        swapchain_loader.destroy_swapchain(swapchain, None);
         logical_device.destroy_device(None);
+        surface_loader.destroy_surface(surface, None);
         debug_utils.destroy_debug_utils_messenger(utils_messenger, None);
         instance.destroy_instance(None);
     };
-    
+
     Ok(())
 }
 
